483d47276d2d689ff17b57904779229f039a3762
kirubel-web <kirubelalemu119@gmail.com>
2024-10-17 21:10:54 +0300

    runtime

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+The operations you will perform will involve breaking down n into its prime factors and summing them up to get the number of operations.
+"""
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
+
