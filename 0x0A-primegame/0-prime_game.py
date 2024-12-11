5c0fdd720455946ac89eb2017feb9164fcbc7758
kirubel-web <kirubelalemu119@gmail.com>
2024-12-11 12:03:53 +0300

    fix

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -10,6 +10,7 @@ def is_prime(n):
             return False
     return True
 
+
 def get_primes_up_to(n):
     """Get list of primes up to n."""
     primes = []
@@ -18,39 +19,36 @@ def get_primes_up_to(n):
             primes.append(i)
     return primes
 
+
 def isWinner(x, nums):
     """
     Determine who wins the prime game more rounds.
-    
     Args:
         x (int): number of rounds
         nums (list): array of n for each round
-    
     Returns:
         str: 'Maria' or 'Ben' or None
     """
     if not nums or x < 1:
         return None
-    
     maria_wins = 0
     ben_wins = 0
-    
+
     # For each round
     for n in nums[:x]:
         if n < 2:
             ben_wins += 1
             continue
-            
         # Get primes up to n
         primes = get_primes_up_to(n)
-        
+
         # If number of primes is even, Ben wins
         # If number of primes is odd, Maria wins
         if len(primes) % 2 == 0:
             ben_wins += 1
         else:
             maria_wins += 1
-    
+
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
